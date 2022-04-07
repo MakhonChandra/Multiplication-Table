b30fcfb78bd488461354fe4758a178b6c320c0a4
Makhon Chandra Barman <103095870+MakhonChandra@users.noreply.github.com>
2022-04-07 12:02:59 -0700

    Add files via upload

---
+++ Multiplication Table.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  7 11:55:38 2022
+
+@author: Makhon Chandra
+"""
+
+n = int(input("Enter The Number: "))
+for i in range(1,11):
+   print (n,"*",i,"=",n*i)
